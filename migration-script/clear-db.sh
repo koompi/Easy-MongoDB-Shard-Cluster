d811533585fa5da724ffaeb9d8e7b4a8405066e4
IsaacJReay <isaacjacksonreay@gmail.com>
2023-10-17 11:12:18 +0700

    Add Clear DB Script

---
+++ migration-script/clear-db.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+DATABASES=`mongosh \
+--quiet \
+--authenticationDatabase admin \
+--tls \
+--tlsCAFile="${NEW_DB_CAFILE}" \
+--tlsCertificateKeyFile="${NEW_DB_PEMKEYFILE}" \
+--username=${NEW_DB_USER} \
+--password=${NEW_DB_PASSWORD} \
+${NEW_DB_HOST}:${NEW_DB_PORT} \
+--eval='show dbs'`
+
+DATABASES=`echo ${DATABASES//[0-9.0-9]} | sed 's/.iB//g'`
+
+for DATABASE in ${DATABASES[@]};
+do
+    mongosh \
+    --quiet \
+    --authenticationDatabase admin \
+    --tls \
+    --tlsCAFile="${NEW_DB_CAFILE}" \
+    --tlsCertificateKeyFile="${NEW_DB_PEMKEYFILE}" \
+    --username=${NEW_DB_USER} \
+    --password=${NEW_DB_PASSWORD} \
+    ${NEW_DB_HOST}:${NEW_DB_PORT}/${DATABASE} \
+    --eval="db.dropDatabase()"
+done
